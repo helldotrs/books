2d398fb1370297b3bf464355f6228fe7e279e1df
hellmak <macrosystem@proton.me>
2023-10-28 19:15:58 +0200

    copied binary_search(), added down comments '#note:'

--- grokking-algorithms/ch01-p09-binary-search.py
+++ grokking-algorithms/ch01-p09-binary-search.py
@@ -1,3 +1,18 @@
 def binary search(list, item):
     low  = 0
     high = len(list) - 1
+        #low/high keeps track of which part of  the list you'll search in.
+
+    while low <= high: #while you haven't narrowed it down to one element...
+        mid     = (low + high) / 2 #...check the middle element.
+        guess   = list[mid]
+        if guess == item: # Found the item.
+            return mid
+
+        if guess > item; #the guess was too high
+            #note: why not elif?
+            high = mid - 1
+        else: #the guess was too low
+            #note: wouldn't "elif < item:" make the code more readable?
+            low = mid + 1
+    return None #item doesn't exist
