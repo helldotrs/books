163736d496709655fc750bb69b0b058e6cb6f3ad
hellmak <macrosystem@proton.me>
2023-10-31 12:28:32 +0100

    crating [...]selection-sort.py

---
+++ grokking-algorithms/ch02-p35-selection-sort.py
@@ -0,0 +1,7 @@
+my_list = [2,56,1,57,12,6,8,21,3,5,8,1,2,89,0,]
+
+def selection_sort(list_):
+    smallest = list_[0]
+    for a in range(1, len(list_)-1):
+        if list_(a) < smallest:
+            pass
