da4201a6dfdaa8eefe872e27747681f0aa7c8f92
hellmak <macrosystem@proton.me>
2023-10-31 12:44:25 +0100

    adding selection_sort() and syntax fixes

--- grokking-algorithms/ch02-p35-selection-sort.py
+++ grokking-algorithms/ch02-p35-selection-sort.py
@@ -1,14 +1,26 @@
-my_list = [2,56,1,57,12,6,8,21,3,5,8,1,2,89,0,]
+my_list = [56,57,12,6,8,21,5,8,89]
 
 def get_smallest_value(list_):
     smallest        = list_[0]
     smallest_index  = 0
 
     for a in range(1, len(list_)-1):
-        if list_(a) < smallest:
+        if list_[a] < smallest:
             smallest        = list_[a]
             smallest_index  = a
 
-    return list_[smallest_index]
+    return list_[smallest_index] #FIXME: wont register "0" as smallest
 
-print(get get_smallest_value(my_list))
+print(get_smallest_value(my_list))
+
+def selection_sort(list_):
+    returnArr = []
+    for a in range(len(list_)): #addubg range(len()) fixes the function, but why?
+        small = get_smallest_value(list_)
+        #books solution is following: returnArr.append(list_.pop(small)), I need to look closer at .pop()
+        returnArr.append(small)
+        list_.remove(small)
+
+    return returnArr
+
+print(selection_sort(my_list))
