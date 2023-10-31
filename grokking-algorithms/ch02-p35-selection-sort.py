133912a98b98aaa8da08374f6b3c7400ef4ed3e8
hellmak <macrosystem@proton.me>
2023-10-31 12:34:08 +0100

    finishing writing get_smallest_value()

--- grokking-algorithms/ch02-p35-selection-sort.py
+++ grokking-algorithms/ch02-p35-selection-sort.py
@@ -1,7 +1,14 @@
 my_list = [2,56,1,57,12,6,8,21,3,5,8,1,2,89,0,]
 
-def selection_sort(list_):
-    smallest = list_[0]
+def get_smallest_value(list_):
+    smallest        = list_[0]
+    smallest_index  = 0
+
     for a in range(1, len(list_)-1):
         if list_(a) < smallest:
-            pass
+            smallest        = list_[a]
+            smallest_index  = a
+
+    return list_[smallest_index]
+
+print(get get_smallest_value(my_list))
