92d3eb1f3329b7a0c604cd0b350110854e8fd561
hellmak <macrosystem@proton.me>
2023-11-02 14:35:31 +0100

    adding comment

--- grokking-algorithms/ch03-p45-recursion-the-stack.py
+++ grokking-algorithms/ch03-p45-recursion-the-stack.py
@@ -5,3 +5,48 @@ def get_factorial(x):
         return x * get_factorial(x-1)
 
 print(get_factorial(3))
+
+#-->
+"""
+get_factorial(x=3):
+    if x == 1:
+        return 1
+    else:
+        return x * (x=2
+                if x == 1:
+                    return 1
+                else:
+                return x * (x=1
+                            return 1)
+        )
+-->
+get_factorial(x=3):
+    if x == 1:
+        return 1
+    else:
+        return x * (x=2
+                if x == 1:
+                    return 1
+                else:
+                return x * (1)
+        )
+        
+-->
+get_factorial(x=3):
+    if x == 1:
+        return 1
+    else:
+        return x * (2)
+        
+-->
+get_factorial(x=3):
+    if x == 1:
+        return 1
+    else:
+        return 3 * (2)
+        
+-->
+return 6
+
+
+"""
