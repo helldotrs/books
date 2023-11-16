83becf248aad48a26db2a3f03192ec6cdcdad916
Rebekah <macrosystem@proton.me>
2023-11-16 14:34:20 +0100

    Update ch04-divide-n-conquer-biggest-square.py

--- grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
+++ grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
@@ -5,10 +5,10 @@ def is_two_item_list(input_list):
     return isinstance(input_list, list) and len(input_list) == 2
 
 def is_rectangle(input_list):
-    return is_two_item_list(input_list) and is_positive_number(input_list[0]) and is_positive_number(input_list[1])
+    return is_two_item_list(input_list) and all(is_positive_number(item) for item in input_list)
 
 def is_square(input_list):
-    def is_rectangle and input_list[0] == input_list[1]
+    return is_rectangle(input_list) and input_list[0] == input_list[1]
 
 # Tests
 """
