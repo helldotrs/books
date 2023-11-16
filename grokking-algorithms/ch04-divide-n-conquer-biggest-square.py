14a8d6d4601934dff376a2e8788a6279ca004fb8
Rebekah <macrosystem@proton.me>
2023-11-16 14:30:22 +0100

    Update ch04-divide-n-conquer-biggest-square.py

--- grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
+++ grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
@@ -1,7 +1,17 @@
 def is_positive_number(x):
     if not isinstance(x, (int, float)) or x <= 0:
-        exit("must be positive number")
-    return x
+        return False
+    return True
+
+def is_two_item_list(input_list):
+    if not isinstance(input_list, list) or  len(input_list) != 2:
+        return False
+    return True
+    
+def is_rectangle(input_list):
+    if not is_two_item_list(input_list) and is_positive_number(input_list[0]) and is_positive_number(input_list[1]):
+        return False
+    return True
 """
 #tests
 print(is_positive_number(5))
