2f567078e06e8d32b0ef3a0d1d6c55fb2ded5f2f
Rebekah <macrosystem@proton.me>
2023-11-16 13:38:47 +0100

    Update ch04-divide-n-conquer-biggest-square.py

--- grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
+++ grokking-algorithms/ch04-divide-n-conquer-biggest-square.py
@@ -1,21 +1,9 @@
-def is_rectangle(input_list):
-    # Check if input_list is a list
-    if not isinstance(input_list, list):
-        raise ValueError("Input must be a list")
+def is_positive_number(x):
+    if not isinstance(value, (int, float)) or value <= 0:
+        exit("must be positive number")
 
-    # Check if the list has exactly two elements
-    if len(input_list) != 2:
-        raise ValueError("A rectangle must contain exactly two values")
-
-    # Check if both elements are either positive integers or floats
-    for element in input_list:
-        if not isinstance(element, (int, float)) or element <= 0:
-            raise ValueError("Elements in the rectangle must be positive integers or floats")
-    
-
-# Example usage:
-try:
-    validate_input([3, 4.5])
-    print("Input is valid.")
-except ValueError as e:
-    print(f"Error: {e}")
+#test
+is_positive_number(5)
+is_positive_number('foo')
+is_positive_number(-1)
+is_positive_number(0)
